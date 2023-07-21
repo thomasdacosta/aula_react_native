0c9b97e648d3e72162f7e6708b93ac8f1ad7963e
Thomás da Costa <thomasdacosta@gmail.com>
2023-07-20 21:00:43 -0300

    first commit

---
+++ aula_04_olamundo/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+      <View
+          style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+          }}>
+        <Text>Olá mundo</Text>
+      </View>
+  )
+}
+export default App;
