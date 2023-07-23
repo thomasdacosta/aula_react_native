afd45022485b44f72ac2d758e1a80ad32a1ce942
Thomás da Costa <thomasdacosta@gmail.com>
2023-07-23 01:59:31 -0300

    fix: ajuste no projeto

--- aula_13_flexbox/App.js
+++ aula_13_flexbox/App.js
@@ -1,20 +1,101 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const ExemploFlexbox = () => {
+    return (
+        <ScrollView>
+            <View style={styles.container}>
+                <View style={styles.section}>
+                    <Text style={styles.text}>Exemplo com flexDirection: 'row'</Text>
+                    <View style={styles.rowContainer}>
+                        <View style={[styles.box, {backgroundColor: 'red'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'green'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'blue'}]}/>
+                    </View>
+                </View>
+
+                <View style={styles.section}>
+                    <Text style={styles.text}>Exemplo com flexDirection: 'column'</Text>
+                    <View style={styles.columnContainer}>
+                        <View style={[styles.box, {backgroundColor: 'red'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'green'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'blue'}]}/>
+                    </View>
+                </View>
+
+                <View style={styles.section}>
+                    <Text style={styles.text}>Exemplo com flexDirection: 'row-reverse'</Text>
+                    <View style={styles.rowReverseContainer}>
+                        <View style={[styles.box, {backgroundColor: 'red'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'green'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'blue'}]}/>
+                    </View>
+                </View>
+
+                <View style={styles.section}>
+                    <Text style={styles.text}>Exemplo com flexDirection: 'column-reverse'</Text>
+                    <View style={styles.columnReverseContainer}>
+                        <View style={[styles.box, {backgroundColor: 'red'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'green'}]}/>
+                        <View style={[styles.box, {backgroundColor: 'blue'}]}/>
+                    </View>
+                </View>
+
+                <View style={styles.section}>
+                    <Text style={styles.text}>Exemplo com flex e flexWrap</Text>
+                    <View style={styles.flexContainer}>
+                        <View style={[styles.box, {flex: 1, backgroundColor: 'red'}]}/>
+                        <View style={[styles.box, {flex: 2, backgroundColor: 'green'}]}/>
+                        <View style={[styles.box, {flex: 3, backgroundColor: 'blue'}]}/>
+                        <View style={[styles.box, {flex: 1, backgroundColor: 'yellow'}]}/>
+                        <View style={[styles.box, {flex: 1, backgroundColor: 'orange'}]}/>
+                        <View style={[styles.box, {flex: 1, backgroundColor: 'purple'}]}/>
+                    </View>
+                </View>
+            </View>
+        </ScrollView>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: 50,
+    },
+    section: {
+        marginBottom: 20,
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    rowContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+    },
+    columnContainer: {
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    rowReverseContainer: {
+        flexDirection: 'row-reverse',
+        justifyContent: 'center',
+    },
+    columnReverseContainer: {
+        flexDirection: 'column-reverse',
+        alignItems: 'center',
+    },
+    flexContainer: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+    },
+    box: {
+        width: 100,
+        height: 100,
+        margin: 10,
+    },
 });
+
+export default ExemploFlexbox;
