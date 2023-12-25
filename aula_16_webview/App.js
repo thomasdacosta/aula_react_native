aec7dbaae541e3481ffb10b169e8658e370ee818
Thomás da Costa <thomasdacosta@gmail.com>
2023-12-25 17:11:01 -0300

    fix: ajuste no projeto

---
+++ aula_16_webview/App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import {WebView} from 'react-native-webview';
+import {StyleSheet} from 'react-native';
+import Constants from 'expo-constants';
+
+export default function App() {
+    return (
+        <WebView
+            style={styles.container}
+            source={{uri: 'https://expo.dev'}}
+        />
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: Constants.statusBarHeight,
+    },
+});
